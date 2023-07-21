3538f4608abc94cd9aab79a3558d9bdaa83f9106
AylarRahimi846 <rahimiaylar846@gmail.com>
2023-07-21 14:38:08 +0430

    fuctionMiddle

---
+++ middle number/005.js
@@ -0,0 +1,6 @@
+let num1=Number(prompt('enter first number'));
+let num2=Number(prompt('enter second number'));
+let num3=Number(prompt('enter third number'));
+
+let score=middle(num1,num2,num3);
+document.write("middle :"+" "+score);

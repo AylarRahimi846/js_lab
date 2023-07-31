492c4df0e67b0f6f4ec1b3782612e8d25d305c85
AylarRahimi846 <rahimiaylar846@gmail.com>
2023-07-31 15:11:08 +0430

    6.zero-plus

---
+++ 6.zero-plus/006.js
@@ -0,0 +1,3 @@
+let num=Number(prompt("enter a number"));
+let answer=zero(num);
+document.write("plus:"+" "+answer);
---
+++ 6.zero-plus/function.js
@@ -0,0 +1,11 @@
+
+function zero(n){
+    let sum=0;
+    for(let i=0; ;i++){
+        sum=sum+n;
+        if(n === 0){
+            break;
+        }
+    }
+    return sum
+}

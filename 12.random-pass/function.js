b6eceee7c91703a1148b8f3c1d1c722a8b780f6f
AylarRahimi846 <rahimiaylar846@gmail.com>
2023-08-05 14:05:39 +0430

    complete.12

--- 12.random-pass/function.js
+++ 12.random-pass/function.js
@@ -17,7 +17,16 @@ function randomPass2() {
 function randomPass3() {
     let thirdPass = "";
     for (let i = i; i < 8; i++) {
-        thirdPass += (String.fromCharCode(math.floor(math.random() * 26 + 97)));
+        thirdPass += (String.fromCharCode(Math.floor(Math.random() * 26 + 97)));
     }
     return thirdPass
+}
+
+function password(){
+    let pass4="";
+    for(let i=0; i<3;i++){
+        pass4+=Math.floor(Math.random()*10)+(String.fromCharCode(Math.floor(Math.random() * 26 + 65)))+(String.fromCharCode(Math.floor(Math.random() * 26 + 97)))
+    }
+
+    return pass4
 }
